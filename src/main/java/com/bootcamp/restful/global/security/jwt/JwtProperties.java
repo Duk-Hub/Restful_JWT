f8be64512089c36ab5ef2d0bb8bd431e5d657339
DongUk <myyds0723@gmail.com>
2026-04-01 16:32:05 +0900

    fix: 누락 setter 추가

--- src/main/java/com/bootcamp/restful/global/security/jwt/JwtProperties.java
+++ src/main/java/com/bootcamp/restful/global/security/jwt/JwtProperties.java
@@ -1,11 +1,13 @@
 package com.bootcamp.restful.global.security.jwt;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.time.Duration;
 
 @Getter
+@Setter
 @ConfigurationProperties(prefix = "jwt")
 public class JwtProperties {
     private String secret;
